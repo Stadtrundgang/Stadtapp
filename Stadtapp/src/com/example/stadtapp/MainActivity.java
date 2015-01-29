f7ede6be4165aef11a80291374072f56c0b495ab
Phoenixza <m.welser@gmx.net>
2015-01-29 08:28:37 +0100

    Erstes Umschreiben des Standtrundgangs für die Organisation

--- Stadtapp/src/com/example/stadtapp/MainActivity.java
+++ Stadtapp/src/com/example/stadtapp/MainActivity.java
@@ -1,34 +1,56 @@
 package com.example.stadtapp;
 
-import android.support.v7.app.ActionBarActivity;
+import com.example.stadtrundgang.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity{
 
+	Button btn_maps;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		findViewById(R.id.btn_gmap).setOnClickListener( new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		        //Inform the user the button has been clicked
+		        Toast.makeText(MainActivity.this, "Button1 clicked.", Toast.LENGTH_SHORT).show(); 
+		        Intent intent = new Intent(MainActivity.this, GoogleMapsActivity.class);
+	            startActivity(intent);
+		    }
+		});
+		
+		findViewById(R.id.btn_photo).setOnClickListener( new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+		        //Inform the user the button has been clicked
+		        Toast.makeText(MainActivity.this, "Button1 clicked.", Toast.LENGTH_SHORT).show(); 
+		        Intent intent = new Intent(MainActivity.this, EventsDemoActivity.class);
+	            startActivity(intent);
+		    }
+		});
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+
+
 }
+	
+	
+
+
