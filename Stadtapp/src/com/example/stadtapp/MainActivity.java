380d092b9fa69a533f8f27e57c15dea5f9caeb8d
Phoenixza <m.welser@gmx.net>
2015-01-29 19:29:28 +0100

    Noch Probleme mit dem senden da httpClient nicht aufgelöst werden kann, trotz apache libarys

--- Stadtapp/src/com/example/stadtapp/MainActivity.java
+++ Stadtapp/src/com/example/stadtapp/MainActivity.java
@@ -1,5 +1,22 @@
 package com.example.stadtapp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.example.stadtapp.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -7,6 +24,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,6 +41,24 @@ public class MainActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		JSONObject jsonDoc = new JSONObject();
+		try {
+		jsonDoc.put("name", "Lolipop"); // erstellt Name=Value‐Paar
+		jsonDoc.put("latitude", 58.0);
+		jsonDoc.put("longitude", 7.88);
+		} catch (JSONException e) {
+		e.printStackTrace();
+		} // end try
+		String body = jsonDoc.toString();
+		JSONperHTTP jsonHTTP = new JSONperHTTP(); // eigene Hilfsklasse
+		String returnPost = jsonHTTP.makeHttpPost("http://141.28.100.212:5984/_utils/index.html", "POST", null,
+		body);
+		
+		 
+		
+		
 		
 		findViewById(R.id.btn_gmap).setOnClickListener( new View.OnClickListener() {
 		    @Override
@@ -47,7 +83,49 @@ public class MainActivity extends Activity{
 	}
 
 
-
+	public static HttpResponse makeUpdateRequest(String uri, Bitmap bmp) {
+	    try {
+	        HttpPut httpPut = new HttpPut(uri);
+	        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+	        bmp.compress(Bitmap.CompressFormat.PNG, 0, stream);
+	        ByteArrayEntity entity = new ByteArrayEntity(stream.toByteArray());
+	        entity.setContentType("image/png");
+	        entity.setChunked(true);
+	        httpPut.setEntity(entity);
+	        return new DefaultHttpClient().execute(httpPut);
+	    } catch (UnsupportedEncodingException e) {
+	        e.printStackTrace();
+	    } catch (ClientProtocolException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
+	
+	private String getParentRevision(String uuid, HttpClient httpClient) {
+		String rev = "";
+		try {
+		    HttpHead head = new HttpHead("http://192.168.56.101/testforms/" + uuid + "/");
+		    HttpResponse resp = httpClient.execute(head);
+		    Header[] headers = resp.getAllHeaders();		  
+		    for (Header header : headers) {		   
+		    if ("Etag".equals(header.getName())) {
+		        StringBuilder arg = new StringBuilder(header.getValue());
+		        if (arg.charAt(0) == '"') {
+		        arg.delete(0, 1);
+		        }
+		        if (arg.charAt(arg.length()-1) == '"'){
+		        arg.delete(arg.length()-1, arg.length());
+		        }
+		        rev = arg.toString();
+		        break;
+		    }
+		    }
+		} catch (Exception ex) {	    
+		}
+		return rev;
+		}
 
 }
 	
